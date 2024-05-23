86cecbe5abc78ceef02adffeae309b3db284cb74
Dipu Haider <dipuhaider@gmail.com>
2024-05-23 11:45:09 +0600

    Create product data

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -4,17 +4,17 @@ import { ProductServices } from "./product.service";
 const createProduct = async (req: Request, res: Response) => {
 
     try{
-        const product = req.body
+        const {product: productData } = req.body;
 
-    //will call service func to send this data
-    const result = await ProductServices.createProductIntoDb(product);
+        //will call service func to send this data
+        const result = await ProductServices.createProductIntoDb(productData);
 
-    //send response
-    res.status(200).json({
-        success: true,
-        message: 'Product is created Successfully.',
-        data: result,
-    });
+        //send response
+        res.status(200).json({
+            success: true,
+            message: 'Product is created Successfully.',
+            data: result,
+        });
     } catch (err){
         console.log(err);
     }
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -5,3 +5,5 @@ const router = express.Router();
 
 //will call controller function
 router.post('/create-product', ProductControllers.createProduct);
+
+export const ProductRoutes = router;

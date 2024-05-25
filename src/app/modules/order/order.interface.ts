5afbc345c81b2557dd12a98dece0263bdf9f7f48
Dipu Haider <dipuhaider@gmail.com>
2024-05-25 10:04:02 +0600

    create and get orders

--- src/app/modules/order/order.interface.ts
+++ src/app/modules/order/order.interface.ts
@@ -1,10 +1,8 @@
 import { Schema, model, connect } from "mongoose";
 
-interface Order {
+export type Order = {
   email: string;
   productId: string;
   price: number;
   quantity: string;
 }
-
-export { Order };

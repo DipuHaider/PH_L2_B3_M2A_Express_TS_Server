8ac69ddbabd8655503c7e7481f87b429c2945016
Dipu Haider <dipuhaider@gmail.com>
2024-05-26 10:34:33 +0600

    deployment fix

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
+import { Server } from "http";
 import app from "./app";
 import config from "./app/config"
 import mongoose from "mongoose";
 
-async function main() {
+let server: Server;
 
+async function main() {
     try{
         await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
+
+        server = app.listen(config.port, () => {
           console.log(`PH_L2_B3_M2A_Express_TS_Server listening on port ${config.port}`)
-        })
+        });
     }catch(err){
         console.log(err)
     }
 }
 
-main();
+main()
 
